a6bbabf1b6fb5cb231dd028383b3c190afbfbf40
Ihor-Lee <Dr.Greezlee@gmail.com>
2024-02-21 11:07:07 +0200

    hwjs2

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,12 @@
-
+function makeArray(firstArray, secondArray, maxLength) {
+  const allArray = firstArray.concat(secondArray);
+  if (allArray.lenght <= maxLength) {
+    return allArray
+  }
+  else {
+    return allArray.slice(0, maxLength)
+  }
+}
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
